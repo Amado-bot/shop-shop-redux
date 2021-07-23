9225efc08aad1a66bba8d5a310e047b2656b1a0d
Amado Gutierres <amadocardenas0@gmail.com>
2021-07-23 16:21:47 -0600

    starter files and global state

---
+++ client/src/components/CategoryMenu/index.js
@@ -0,0 +1,66 @@
+import React, { useEffect } from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { QUERY_CATEGORIES } from '../../utils/queries';
+import { idbPromise } from '../../utils/helpers';
+
+function CategoryMenu() {
+
+
+
+
+  //retrieve current global state and dispatch method to update state
+  const [state, dispatch] = useStoreContext();
+  //only using categories array from the globalstate -- destructure
+  const { categories } = state;
+  //note that we want to destructure the loading variable for use with indexedDB
+  const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+  useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
+    if (categoryData) {
+      dispatch({
+        type: UPDATE_CATEGORIES,
+        categories: categoryData.categories
+      });
+      //write to indexedDB categories object store 
+      categoryData.categories.forEach(category => {
+        idbPromise('categories', 'put', category);
+      });
+    } else if (!loading) {
+      idbPromise('categories', 'get').then(categories => {
+        dispatch({
+          type: UPDATE_CATEGORIES,
+          categories: categories
+        });
+      });
+    }
+  }, [categoryData, loading, dispatch]);
+
+  const handleClick = id => {
+    // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
+    dispatch({
+      type: UPDATE_CURRENT_CATEGORY,
+      currentCategory: id
+    });
+  };
+
+  return (
+    <div>
+      <h2>Choose a Category:</h2>
+      {categories.map(item => (
+        <button
+          key={item._id}
+          onClick={() => {
+            handleClick(item._id);
+          }}
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default CategoryMenu;
